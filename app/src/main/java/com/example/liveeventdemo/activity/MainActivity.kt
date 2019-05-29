77411a655591a1f839e168f0292845c3b05c4ae4
zhengxin <xinzheng@shein.com>
2019-05-29 22:28:49 +0800

    feat(Demo):LiveData简单使用

--- app/src/main/java/com/example/liveeventdemo/activity/MainActivity.kt
+++ app/src/main/java/com/example/liveeventdemo/activity/MainActivity.kt
@@ -1,27 +1,47 @@
 package com.example.liveeventdemo.activity
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
 import com.example.liveeventdemo.R
 import com.example.liveeventdemo.livedatabus1.VersionFirstActivity
 import com.example.liveeventdemo.livedatabus2.VersionSecondActivity
 import com.example.liveeventdemo.util.routeTo
+import com.example.liveeventdemo.util.showToast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+    private val mTestLiveData = MutableLiveData<String>()
+
     override fun initLayoutResID() = R.layout.activity_main
 
     override fun initListener() {
+        MutableLiveData<String>()
+        //源码
+        bt_source.setOnClickListener {
+            mTestLiveData.value = "发布消息"
+            mTestLiveData.postValue("post")
+        }
+
+        //版本一
         bt_version1.setOnClickListener {
             routeTo<VersionFirstActivity>()
         }
 
+        //版本二
         bt_version2.setOnClickListener {
             routeTo<VersionSecondActivity>()
         }
 
+        //版本三
         bt_version3.setOnClickListener {
             routeTo<VersionFirstActivity>()
         }
+    }
+
+    override fun initObserve() {
+        super.initObserve()
+        mTestLiveData.observe(this, Observer {
+            showToast("LiveData事件订阅---$it")
+        })
     }
 }
